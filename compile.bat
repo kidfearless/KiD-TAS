31ef335a985d4702777c8d2765650fd14f9c7e60
KiD Fearless <kidfearless1@gmail.com>
2020-02-19 20:27:18 -0700

    Added windows compile script

---
+++ compile.bat
@@ -0,0 +1,4 @@
+cd %~dp0
+scripting\spcomp.exe scripting\xutax-autostrafe.sp -iscripting\include\ -ocompiled\xutax-autostrafe.smx
+scripting\spcomp.exe scripting\autostrafe.sp -iscripting\include\ -ocompiled\autostrafe.smx
+scripting\spcomp.exe scripting\kid-tas.sp -iscripting\include\ -ocompiled\kid-tas.smx
